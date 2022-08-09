72ff79bd64fdad8a50e0cbecd950bd715a5168e1
harsha javalkar <HarshaJavalkar>
2022-08-09 11:06:05 +0530

    first(create): created mock app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TemplateComponent } from './template/template.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TemplateComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
